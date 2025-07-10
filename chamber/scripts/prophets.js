770524f3843a2ff7afb96157a5707d3f00ad65c4
NephiImo <imonephi@gmail.com>
2025-07-10 22:14:04 +0100

    1st disco push

---
+++ chamber/scripts/prophets.js
@@ -0,0 +1,19 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+    });
+}
